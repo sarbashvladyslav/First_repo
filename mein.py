a66469e2510bad158340e0eb0f6d8b25a703beee
sarbashvladyslav <sarbash.vladyslav@gmail.com>
2025-08-06 09:58:00 +0200

    first_hw

--- mein.py
+++ mein.py
@@ -1,3 +1,32 @@
-print("Hello World")
-print("I need a new laptop")
-print("I need a new phone")
+import numpy as np
+
+#Solving the system in matrix form
+
+#a = np.matrix("1,1,1;0.05,0.07,0;0.05,0,0.06")
+#b = np.matrix("50000;2250;1400")
+#a_inv = np.linalg.inv(a)
+#x = a_inv.dot(b)
+#print(x)
+
+#solv method
+
+#a = np.matrix("1,1,1;0.05,0.07,0;0.05,0,0.06")
+#b = np.matrix("50000;2250;1400")
+#x = np.linalg.solve(a,b)
+#print(x)
+
+#p.array + operator @
+
+a = np.array([
+    [1, 1, 1],
+    [0.05, 0.07, 0],
+    [0.05, 0, 0.06]
+])
+
+b = np.array([50000, 2250, 1400])
+x = np.linalg.solve(a, b)
+print(x)
+
+b_check = a @ x
+print(b_check)
+print(np.allclose(b, b_check))
